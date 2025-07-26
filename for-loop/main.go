7c5dba7f6824b31692ce87d8adbee847b0fe04c7
deepanshu <deepanshukamlesh@gamil.com>
2025-07-26 22:15:10 +0530

    for loop doing it all...

---
+++ for-loop/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 1; i++ {
+		fmt.Println("Numbers is :", i)
+	}
+
+	counter := 0 
+	for {  
+		fmt.Println("Infinite Loop")
+		counter++
+		if counter == 1 {
+			break
+		}
+	}
+
+	numbers := []int{11, 42, 83, 14, 75}   // Slice of integers
+	// Using range to iterate over the slice
+	for index, value := range numbers {
+		fmt.Printf("Index of Numbers is %d, and value is %d\n", index, value)
+	}
+
+	data := "Hello, world!"
+	for index, value := range data {        
+		fmt.Printf("Index of Data is %d, and value is %c\n", index, value)
+	}
+
+}
